fca77611c9bad69a91b57e87009037706990fc59
Javier Saura <j.saura@fiberpachs.com>
2020-07-20 14:38:01 +0200

    assets

--- src/main.js
+++ src/main.js
@@ -7,6 +7,8 @@ import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+import './assets/css/main.styl'
+
 Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
