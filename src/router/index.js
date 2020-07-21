91581d1ca60320874b20cc42965f842ec4cee6a2
Javier Saura <j.saura@fiberpachs.com>
2020-07-21 08:31:18 +0200

    Bug fix in vue router

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Router from 'vue-router'
 
-Vue.use(VueRouter)
+Vue.use(Router)
 
 const routerOptions = [
   { path: '/', name: 'Home' },
