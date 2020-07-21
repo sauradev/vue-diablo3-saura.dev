23019fd7e35fa8f2681e25faaefc2138afcd2474
Javier Saura <j.saura@fiberpachs.com>
2020-07-21 14:51:00 +0200

    Listado de Héroes

--- src/views/Profile/MainBlock/Index.vue
+++ src/views/Profile/MainBlock/Index.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="grid-container">
     <div class="grid-item item-left">
-      <h1>Izquierda</h1>
-      <TopHeroes v-if="hasHeroes" :heroes="topHeroes"/>
+      <TopHeroes v-if="hasHeroes" :heroes="topHeroes" />
+      <HeroesList v-if="hasHeroesList" :heroes="heroesList" />
     </div>
     <div class="grid-item item-right">
       <h1>Derecha</h1>
@@ -11,12 +11,15 @@
 </template>
 
 <script>
-// Importamos
 import TopHeroes from './TopHeroes/Index'
+import HeroesList from './HeroesList/Index'
 
 export default {
   name: 'MainBlock',
-  components: { TopHeroes },
+  components: {
+    TopHeroes,
+    HeroesList
+  },
   props: {
     profileData: {
       type: Object,
@@ -24,36 +27,40 @@ export default {
     }
   },
   computed: {
-    // Comprobamos que hay héroes
     hasHeroes () {
       return this.profileData.heroes.length > 0
     },
-    // Devolvemos los 3 primeros
     topHeroes () {
       return this.profileData.heroes.slice(0, 3)
+    },
+    hasHeroesList () {
+      return this.profileData.heroes.length > 3
+    },
+    heroesList () {
+      return this.profileData.heroes.slice(3, this.profileData.heroes.length)
     }
   }
 }
 </script>
 
 <style lang="stylus">
+.grid-container
+  display grid
+  grid-template-columns 1fr
+
+.grid-item
+  &.item-left
+    grid-column span 1
+
+  &.item-right
+    grid-column span 1
+
+@media (min-width: 992px)
   .grid-container
-    display grid
-    grid-template-columns 1fr
-
-    .grid-item
-      &.item-left
-        grid-column span 1
-
-      &.item-right
-        grid-column span 1
-
-  @media (min-width: 992px)
-    .grid-container
       display grid
       grid-template-columns repeat(6, 1fr)
 
-      .grid-item
-        &.item-left
-          grid-column span 4
+.grid-item
+  &.item-left
+    grid-column span 4
 </style>
