742d246fc12acd2b380f7e5f8106f40cff48f37f
Javier Saura <j.saura@fiberpachs.com>
2020-07-21 08:41:59 +0200

    Profile View

--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,7 @@ import Vuex from 'vuex'
 
 import oauth from './modules/oauth'
 import loading from './modules/loading'
+import error from './modules/error'
 
 Vue.use(Vuex)
 
@@ -15,6 +16,7 @@ export default new Vuex.Store({
   },
   modules: {
     oauth,
-    loading
+    loading,
+    error
   }
 })
