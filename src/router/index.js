85b2cf48e39bcdbf8f02feabc65e7a23439aafbf
Javier Saura <j.saura@fiberpachs.com>
2020-07-22 12:18:07 +0200

    Change app title

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,23 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 const routerOptions = [
-  { path: '/', name: 'Home' },
+  {
+    path: '/',
+    name: 'Home',
+    meta: {
+      title: 'Diablo3 - Vue.js - Javier Saura',
+      metaTags: [
+        {
+          name: 'description',
+          content: 'App realializada por Javier Saura en Vue.js'
+        },
+        {
+          property: 'og:description',
+          content: 'App realializada por Javier Saura en Vue.js'
+        }
+      ]
+    }
+  },
   { path: '/region/:region/profile/:battleTag', name: 'Profile' },
   { path: '/region/:region/profile/:battleTag/hero/:heroId', name: 'Hero' },
   { path: '/about', name: 'About' },
